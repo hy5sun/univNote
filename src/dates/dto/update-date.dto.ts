bd2041ebc4bdd17058fbbae5bc72d3c1c4143b24
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-08-20 06:39:03 +0900

    feat: add IsNotEmpty

--- src/dates/dto/update-date.dto.ts
+++ src/dates/dto/update-date.dto.ts
@@ -1,10 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateDateDto {
+  @IsNotEmpty({ message: '수정할 일정을 필수로 입력해야 합니다.' })
   @IsString({ message: '일정은 문자열이어야 합니다.' })
   schedule: string;
 
-  @IsNotEmpty({ message: '날짜는 필수 입력해야 합니다.' })
+  @IsNotEmpty({ message: '날짜를 필수로 입력해야 합니다.' })
   @IsString({ message: '일정은 문자열 형식이어야 합니다.' })
   updateDate: string;
 }
