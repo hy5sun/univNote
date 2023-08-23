634e89ec7b0585e77d111f1f5d7031684d2a3744
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-08-23 09:46:53 +0900

    fix: delete ReviewEntity

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -3,7 +3,6 @@ import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityEntity } from './entities/activity.entity';
-import { ReviewEntity } from './entities/review.entity';
 import { CacheModule } from '@nestjs/cache-manager';
 import { UsersModule } from 'src/users/users.module';
 
@@ -11,7 +10,7 @@ import { UsersModule } from 'src/users/users.module';
   imports: [
     UsersModule,
     CacheModule.register({ isGlobal: true }),
-    TypeOrmModule.forFeature([ActivityEntity, ReviewEntity]),
+    TypeOrmModule.forFeature([ActivityEntity]),
   ],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
